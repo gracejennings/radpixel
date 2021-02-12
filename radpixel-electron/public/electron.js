f4bcb0530d9a698da100e3d1506e0be21b2dc13d
gracejennings <grace.e.jennings@vanderbilt.edu>
2021-02-12 08:49:15 -0600

    changes from article

--- radpixel-electron/public/electron.js
+++ radpixel-electron/public/electron.js
@@ -5,6 +5,8 @@ const url = require('url');
 const { app } = electron;
 const { BrowserWindow } = electron;
 
+const { ipcMain } = require('electron');
+
 let mainWindow;
 
 function createWindow() {
@@ -38,3 +40,54 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+// ------------------- set up event listeners here --------------------
+
+// temporary variable to store data while background
+// process is ready to start processing
+let cache = {
+	data: undefined,
+};
+
+// a window object outside the function scope prevents
+// the object from being garbage collected ???
+let hiddenWindow;
+
+// This event listener will listen for request
+// from visible renderer process
+ipcMain.on('START_BACKGROUND_VIA_MAIN', (event, args) => {
+	const backgroundFileUrl = url.format({
+		pathname: path.join(__dirname, `../background/background.html`),
+		protocol: 'file:',
+		slashes: true,
+	});
+	hiddenWindow = new BrowserWindow({
+		show: false,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
+	hiddenWindow.loadURL(backgroundFileUrl);
+
+	hiddenWindow.webContents.openDevTools();
+
+	hiddenWindow.on('closed', () => {
+		hiddenWindow = null;
+	});
+
+	cache.data = args.number;
+});
+
+// This event listener will listen for data being sent back
+// from the background renderer process
+ipcMain.on('MESSAGE_FROM_BACKGROUND', (event, args) => {
+	mainWindow.webContents.send('MESSAGE_FROM_BACKGROUND_VIA_MAIN', args.message);
+});
+
+ipcMain.on('BACKGROUND_READY', (event, args) => {
+
+	event.reply('START_PROCESSING', {
+		data: cache.data,
+	});
+});
+
